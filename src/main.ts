88624bc8598386950379aa5abf78bb9f15ef1995
do-ald533 <doouglas.aldred533@gmail.com>
2024-10-05 08:05:10 -0300

    feat: creating dev enviorment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import nocache from 'nocache';
+import compression from 'compression';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableShutdownHooks();
+  app.use(helmet());
+  app.use(nocache());
+  app.use(compression());
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      errorHttpStatusCode: 422,
+    }),
+  );
+
+  const configService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Metro SP Core API')
+    .setDescription(
+      'Referências para as chamadas de API',
+    )
+    .setVersion('1.0')
+    .addApiKey()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const port = configService.get<string>('PORT') || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+
   await app.listen(3000);
 }
 bootstrap();
